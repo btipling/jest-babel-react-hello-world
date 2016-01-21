f9ffdf55fdb21aa6cfc9f8091cc3e36b5e9fa993
Bjorn Tipling <bjorn@imgur.com>
2016-01-21 13:46:32 -0800

    Don't need any of this.

--- src/__tests__/testcomponent.js
+++ src/__tests__/testcomponent.js
@@ -1,14 +1,7 @@
 import '../tests/init';
-import React from 'react';
-import { findDOMNode } from 'react-dom';
 import Component from '../public/js/Component';
-import { findRenderedComponentWithType,
-         renderIntoDocument,
-         Simulate,
-         scryRenderedDOMComponentsWithClass } from 'react-addons-test-utils';
 
 describe('Component', () => {
-
   it('is a Component', () => {
     expect(typeof Component).toEqual('function');
   });
